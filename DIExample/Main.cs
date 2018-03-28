b51b0e312c430bf563eeb473cba450da12b65413
Lei (Leo) Zhao <Leo@zhaodeveloper.com>
2018-03-29 10:46:28 +1100

    Add files

---
+++ DIExample/Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DIExample
+{
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlServerDal dal = new SqlServerDal();
+            Order order = new Order(dal);
+
+            order.Add();
+
+            Console.Read();
+        }
+    }
+
+
+}
---
+++ DIExample/Models.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIExample
+{
+    class Models
+    {
+
+    }
+    public interface IDataAccess
+    {
+        void Add();
+    }
+
+    public class SqlServerDal : IDataAccess
+    {
+        public void Add()
+        {
+            Console.WriteLine("Add a order");
+        }
+    }
+
+    public class Order
+    {
+        private IDataAccess _ida;
+        public Order(IDataAccess ida)
+        {
+            _ida = ida;
+        }
+
+        public void Add()
+        {
+            _ida.Add();
+        }
+    }
+
+}
